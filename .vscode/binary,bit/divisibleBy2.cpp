c2aaa32f37bef01306c499e1e81880c0ec66b443
Vignesh dandu <VIGNESHDANDU8355@GMAIL.COM>
2026-03-12 13:50:56 +0000

    Add Binary to Decimal and Decimal to Binary conversion programs

---
+++ .vscode/binary,bit/divisibleBy2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int d;
+    int sum = 0 ;
+    cout << "Enter a number : " << endl;
+    cin >> d ;
+
+    while(d!=0){
+        sum += d%2
+        sum *=10
+        
+    }
+
+    return 0;
+}
